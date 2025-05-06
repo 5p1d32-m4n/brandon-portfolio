ecdf1694afe8a6a6535e8eb01af153fd1b5c2d9e
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-05-06 02:16:09 -0400

    pending connection string.

--- api/projects.js
+++ api/projects.js
@@ -5,11 +5,11 @@ export default async function handler(req, res) {
     if (req.method === 'GET') {
         try{
             const {rows: projects} = await sql `SELECT * FROM projects ORDER BY id ASC;`;
+            res.status(200).json(projects); // Send the projects as a JSON response
         }catch(e){
             console.error('Database error:', e);
             res.status(500).json({message: 'Error fetching projects', error: e.message});
         }
-        res.status(200).json(projects); // Send the projects as a JSON response
     } else {
         res.setHeader('Allow', ['GET']);
         res.status(405).json({ message: `Method ${req.method} Not Allowed` });
