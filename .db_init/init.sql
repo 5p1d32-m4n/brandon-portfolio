c70ed2b9db23e51346decf82c9d46c70964226df
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-05-09 23:09:51 -0400

    new sql changes.

--- .db_init/init.sql
+++ .db_init/init.sql
@@ -4,14 +4,29 @@ CREATE TABLE IF NOT EXISTS projects (
     title VARCHAR(255) NOT NULL,
     slug VARCHAR(255) UNIQUE NOT NULL,
     description TEXT,
+    short_description TEXT, -- Optional: For future use
+    project_type VARCHAR(50) DEFAULT 'Default', -- Optional: For future use
+    status VARCHAR(50) DEFAULT 'In Progress', -- Optional: For future use
     hero_image_url VARCHAR(255),
     logo_image_url VARCHAR(255),
+    tech_stack TEXT, -- Optional: For future use
     repo_link VARCHAR(255),
     live_demo_url VARCHAR(255),
     view_case_href VARCHAR(255), -- For "View Case" type links
     link_color VARCHAR(50),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS project_images(
+    id SERIAL PRIMARY KEY,
+    project_id INT NOT NULL,
+    image_public_id VARCHAR(255) NOT NULL, -- Cloudianary public ID for this image
+    description TEXT,
+    alt_text VARCHAR(255),
+    display_order INT DEFAULT 0,
+    uploadaded_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
+)
 
 -- Insert initial project data
 -- Note: Apostrophes in text are escaped by doubling them (e.g., Wargamer''s Alley)
@@ -39,8 +54,25 @@ VALUES (
     '#F7BF00'
 ) ON CONFLICT (slug) DO NOTHING;
 
+
 -- Add more projects here if you like:
 -- INSERT INTO projects (title, slug, ...) VALUES (...) ON CONFLICT (slug) DO NOTHING;
 
 -- Grant privileges if needed, though the default user usually has them on the owned DB.
 -- GRANT ALL PRIVILEGES ON TABLE projects TO postgres; -- 'postgres' or your user
+
+
+-- Assuming WarMiniPricer was inserted into 'projects' and its 'id' is 1
+-- (You might need to query the ID or know it if starting fresh)
+INSERT INTO project_images (project_id, image_public_id, description, alt_text, display_order) VALUES
+    (1, 'minis-list_x0pcqh', 'Home view', 'WarMiniPricer home screenshot', 0),
+    (1, 'mini-detail_maj080', 'Miniature detail view', 'Screenshot of war mini details', 1),
+    (1, 'portfolio/warminipricer/mobile_view_01', 'Mobile interface', 'WarMiniPricer on a mobile device', 2);
+	(1, 'modalStoreOrder_j3q9gg', 'Store Order Modal', 'Store order form', 3);
+	(1, 'modalContactSupplier_mkhdbh', 'Supplier Contact Modal', 'Supplier Contact form', 4);
+	(1, 'modalDeliveryOrder_k6dstt', 'Delivery Order Modal', 'Delivery order form', 5);
+
+-- Assuming STG-ERP was inserted into 'projects' and its 'id' is 2
+INSERT INTO project_images (project_id, image_public_id, description, alt_text, display_order) VALUES
+    (2, 'portfolio/stg_erp/overview_dashboard', 'ERP System Overview', 'STG-ERP dashboard overview', 0),
+    (2, 'portfolio/stg_erp/module_finance', 'Finance Module', 'Screenshot of STG-ERP finance module', 1);
